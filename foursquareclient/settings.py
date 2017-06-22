e15ca754ee59282586b8cad3d75ed9fda64c6032
Sinan Gök <SG@Sinans-MacBook-Air.local>
2017-06-22 15:09:58 +0300

    Make migrations for new model: Previous Searches

--- foursquareclient/settings.py
+++ foursquareclient/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'foursquaresearch.apps.FoursquaresearchConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
