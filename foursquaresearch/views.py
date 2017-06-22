49961c17291a61eb39f7acd67633e56ecdb4d673
Sinan Gök <SG@Sinans-MacBook-Air.local>
2017-06-22 15:27:53 +0300

    Create proto search_bar view and template

--- foursquaresearch/views.py
+++ foursquaresearch/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    
     return HttpResponse("The Ultimate Foursquare Client will be here!")
+
+def search_bar(request):
+    return render(request, 'foursquaresearch/search_bar.html', {})
