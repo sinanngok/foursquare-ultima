b80f4509ea5103a8757cb13a8c0ec0de2350aae2
Sinan Gök <goksinan@sabanciuniv.edu>
2017-07-28 13:38:24 +0300

    Changes in join method

--- foursquaresearch/utils.py
+++ foursquaresearch/utils.py
@@ -14,14 +14,15 @@ def get_foursquare_results(location, what_to_look, venues, error, is_searched, l
     SECRET_TOKEN = "&client_secret="
     CLIENT_SECRET = "L5RZFRA1K2KPH33H12BFD3MECOJKEBIJSLP14KXYRYW3A5AF"
     LOCATION_TYPE = "&near="
-    proto_url = ''.join([API_ADRESS, CLIENT_ID, SECRET_TOKEN, CLIENT_SECRET, LOCATION_TYPE])
+    proto_url = f'{API_ADRESS}{CLIENT_ID}{SECRET_TOKEN}{CLIENT_SECRET}{LOCATION_TYPE}'
+        #f"My cool string is called {name}."
 
     #VERSION date: 20.06.2017 --> 20170620
     VERSION = "&v=20170620"
     SEARCH_PLACE = "&m=foursquare"
-    post_url = ''.join([VERSION, SEARCH_PLACE])
+    post_url = f'{VERSION}{SEARCH_PLACE}'
 
-    url = ''.join([proto_url, location, "&query=", what_to_look, post_url])
+    url = f'{proto_url}{location}&query={what_to_look}{post_url}'
 
     response = requests.get(url)
     data = response.json()
